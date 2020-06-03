669c1a6dfd4b1f29de8e4752c4d05c0beb3b7efa
Nickolazz1 <elanir358@gmail.com>
2020-06-03 23:07:04 +0300

    less 4 components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+/*
 function App() {
   return (
     <div className="App">
@@ -25,5 +25,33 @@ function App() {
     </div>
   );
 }
+*/
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+
+          <p>Hello Worlda</p>
+          
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
 
 export default App;
