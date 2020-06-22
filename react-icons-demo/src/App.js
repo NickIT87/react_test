62a81fbff5df694f315944987bac01fc73b66279
Nickolazz1 <elanir358@gmail.com>
2020-06-22 15:04:37 +0300

    less2 icons (practical react)

--- react-icons-demo/src/App.js
+++ react-icons-demo/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { IconContext } from 'react-icons'
+import { FaReact } from 'react-icons/fa'
+import { MdAlarm } from 'react-icons/md' 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IconContext.Provider value={{color: 'blue', size: '5rem'}}>
+      <div className="App">
+        <FaReact color='purple' size='10rem' />
+        <MdAlarm/>
+        <FaReact/>
+      </div>
+    </IconContext.Provider>
   );
 }
 
