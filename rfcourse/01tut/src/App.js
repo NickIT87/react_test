62ede12a4fd86d37cd62aeabc62c3c215f68ceb2
Nickolazz1 <elanir358@gmail.com>
2022-02-05 17:30:59 +0200

    ch2 complete

---
+++ rfcourse/01tut/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const handleNameChange = () => {
+    const names = ['Bob', 'Kevin', 'Dave']
+    const int = Math.floor(Math.random() * 3)
+    return names[int]
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello { handleNameChange() }!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ rfcourse/readme.txt
@@ -0,0 +1,4 @@
+
+Dave Gray
+
+https://www.youtube.com/watch?v=RVFAyFWO4go
