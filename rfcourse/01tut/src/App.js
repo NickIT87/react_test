c4467591634a43fae606d0afffc3fed52c505340
Nickolazz1 <elanir358@gmail.com>
2022-02-06 18:59:32 +0200

    l3,4 complete

--- rfcourse/01tut/src/App.js
+++ rfcourse/01tut/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+
 
 function App() {
-
-  const handleNameChange = () => {
-    const names = ['Bob', 'Kevin', 'Dave']
-    const int = Math.floor(Math.random() * 3)
-    return names[int]
-  }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello { handleNameChange() }!
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Content />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
