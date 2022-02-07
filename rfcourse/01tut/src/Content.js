5f9659b94cb2ffab1b2b11028bb0edaff5dec8c8
Nickolazz1 <elanir358@gmail.com>
2022-02-07 16:40:27 +0200

    l5 event

--- rfcourse/01tut/src/Content.js
+++ rfcourse/01tut/src/Content.js
@@ -6,11 +6,27 @@ const Content = () => {
         return names[int]
     }
 
+    const handleClick = () => {
+        console.log('Clicked')
+    }
+
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked`)
+    }
+
+    const handleClick3 = (e) => {
+        console.log(e)
+        console.log(e.target.innerText)
+    }
+
     return (
         <main>
-            <p>
+            <p onDoubleClick={handleClick}>
                 Hello {handleNameChange()}
             </p>
+            <button onClick={handleClick}>Click It</button>
+            <button onClick={() => handleClick2('Dave')}>Click It</button>
+            <button onClick={(e) => handleClick3(e)}>Click It</button>
         </main>
     )
 };
