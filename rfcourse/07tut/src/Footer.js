ceed982353d866ed810549b1a8f02960d98fc5d5
Nickolazz1 <elanir358@gmail.com>
2022-02-13 15:02:30 +0200

    l8 complete

--- rfcourse/07tut/src/Footer.js
+++ rfcourse/07tut/src/Footer.js
@@ -1,9 +1,10 @@
-const Footer = () => {
+const Footer = ({length}) => {
     const today = new Date();
 
     return (
         <footer>
-            <p>Copyright &copy; {today.getFullYear()}</p>
+            {/*<p>Copyright &copy; {today.getFullYear()}</p>*/}
+            <p>{length} List {length === 1 ? "item":"items"}</p>
         </footer>
     )
 }
