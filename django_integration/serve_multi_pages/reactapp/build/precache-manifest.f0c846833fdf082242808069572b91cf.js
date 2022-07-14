877da0d974a2329bd4ee2ce4bd7ff0f2d88f58e6
Nickolazz1 <elanir358@gmail.com>
2022-07-14 14:28:47 +0300

    serve react multiple pages

---
+++ django_integration/serve_multi_pages/reactapp/build/precache-manifest.f0c846833fdf082242808069572b91cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6376f41682b635240c2",
+    "url": "/static/css/main.a630cf3a.chunk.css"
+  },
+  {
+    "revision": "b6376f41682b635240c2",
+    "url": "/static/js/main.b6376f41.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "acbb317a8ffd2a43496d",
+    "url": "/static/js/2.acbb317a.chunk.js"
+  },
+  {
+    "revision": "43928d61158871188870",
+    "url": "/static/js/runtime~main.43928d61.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "50d91ec01c5b19e75f095de9f6608723",
+    "url": "/index.html"
+  }
+];
