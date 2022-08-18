357eb01b87b9417e2592a252e7345eedbb049caf
Nickolazz1 <elanir358@gmail.com>
2022-08-19 00:12:12 +0300

    mui demo

---
+++ MUI/react-mui-demo/src/components/MuiTypography.tsx
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material"
+
+export const MuiTypography = () => {
+  return (
+    <div>
+        <Typography variant='h1'>h1</Typography>
+        <Typography variant='h2'>h2</Typography>
+        <Typography variant='h3'>h3</Typography>
+        <Typography variant='h4'>h4</Typography>
+        <Typography variant='h5'>h5</Typography>
+        <Typography variant='h6'>h6</Typography>
+    </div>
+  )
+}
