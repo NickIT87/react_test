93a73d34b87ca91e01e4bef4b86a93daa9911801
Nickolazz1 <elanir358@gmail.com>
2022-08-19 11:40:55 +0300

    typography

--- MUI/react-mui-demo/src/components/MuiTypography.tsx
+++ MUI/react-mui-demo/src/components/MuiTypography.tsx
@@ -3,12 +3,20 @@ import { Typography } from "@mui/material"
 export const MuiTypography = () => {
   return (
     <div>
-        <Typography variant='h1'>h1</Typography>
-        <Typography variant='h2'>h2</Typography>
-        <Typography variant='h3'>h3</Typography>
-        <Typography variant='h4'>h4</Typography>
-        <Typography variant='h5'>h5</Typography>
-        <Typography variant='h6'>h6</Typography>
+        <Typography variant='h1'>h1 heading</Typography>
+        <Typography variant='h2'>h2 heading</Typography>
+        <Typography variant='h3'>h3 heading</Typography>
+        <Typography variant='h4' component='h2'>h4 heading</Typography>
+        <Typography variant='h5'>h5 heading</Typography>
+        <Typography variant='h6'>h6 heading</Typography>
+
+        <Typography variant='subtitle1'>sub title 1</Typography>
+        <Typography variant='subtitle2'>sub title 2</Typography>
+
+        <Typography variant='body1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique deleniti laboriosam a ducimus saepe optio ex, adipisci explicabo? Perferendis temporibus sint incidunt impedit vero debitis esse iure mollitia, magni quisquam?</Typography>
+        <Typography variant='body2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur ad deserunt error aliquam, quibusdam veniam doloremque a omnis harum velit et veritatis odio sint, sunt incidunt explicabo distinctio necessitatibus sed?</Typography>
+
+
     </div>
   )
 }
