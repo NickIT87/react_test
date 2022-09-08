e17788718a413d615b2c53b0ccaa78a669c3e552
NickIT87 <elanir358@gmail.com>
2022-09-08 10:21:25 +0300

    text field

--- MUI/react-mui-demo/src/components/MuiTextField.tsx
+++ MUI/react-mui-demo/src/components/MuiTextField.tsx
@@ -1,10 +1,14 @@
-import { Stack, TextField } from "@mui/material"
+import { Stack, TextField, InputAdornment } from "@mui/material"
+import { useState } from "react"
 
 export const MuiTextField = () => {
+  const [value, setValue] = useState("")
+
   return (
     <Stack spacing={4}>
         <Stack direction='row' spacing={2}>
-            <TextField label="Name" variant="outlined"/>
+            <TextField label="Name" id="nt" variant="outlined" error/>
+            <TextField label="Name" id="nt" variant="outlined"/>
             <TextField label="Name" variant="filled"/>
             <TextField label="Name" variant="standard"/>
         </Stack>
@@ -12,8 +16,31 @@ export const MuiTextField = () => {
             <TextField label="Small secondary" size="small" color="secondary" />
         </Stack>
         <Stack direction='row' spacing={2}>
-            <TextField label="Form Input" required/>
-            <TextField label="Form Input" helperText="Do not share your password" />
+          <TextField label="Form Input" required disabled/>
+          <TextField 
+            label="Form Input" 
+            required
+            onChange={ e => setValue(e.target.value) } 
+            error={!value}
+            helperText={
+              !value ? 'Required' : 'Do not share your password'
+            }
+          />
+          <TextField 
+            label="Password"
+            type='password'
+            helperText="Do not share your password with anyone else"
+            required
+          />
+          <TextField label="Read only" InputProps={{ readOnly: true }}/>
+        </Stack>
+        <Stack direction='row' spacing={2}>
+          <TextField label="Amount" InputProps={{
+            startAdornment: <InputAdornment position="start">$</InputAdornment>
+          }}/>
+          <TextField label="Weight" InputProps={{
+            endAdornment: <InputAdornment position="end">kg</InputAdornment>
+          }}/>
         </Stack>
     </Stack>
   )
