66594b1f32bb3e6ab0a11a1bcec0398914cab242
NickIT87 <elanir358@gmail.com>
2022-09-19 18:05:24 +0300

    radio button

--- MUI/react-mui-demo/src/App.tsx
+++ MUI/react-mui-demo/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
-import { MuiSelect } from './components/MuiSelect';
+import { MuiRadioButton } from './components/MuiRadioButton';
+//import { MuiSelect } from './components/MuiSelect';
 //import { MuiTextField } from './components/MuiTextField';
 //import { MuiButton } from './components/MuiButton'
 //import { MuiTypography } from './components/MuiTypography';
@@ -7,10 +8,11 @@ import { MuiSelect } from './components/MuiSelect';
 function App() {
   return (
     <div className="App">
-      {/* <MuiTypography /> */}
-      {/* <MuiButton /> */}
-      {/* <MuiTextField /> */}
-      <MuiSelect />
+      {/* <MuiTypography/> */}
+      {/* <MuiButton/> */}
+      {/* <MuiTextField/> */}
+      {/* <MuiSelect/> */}
+      <MuiRadioButton/>
     </div>
   );
 }
