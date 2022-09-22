c6b80285eb32b9ba1fff055296c40e5579a54a59
NickIT87 <elanir358@gmail.com>
2022-09-22 17:15:01 +0300

    rating component

--- MUI/react-mui-demo/src/App.tsx
+++ MUI/react-mui-demo/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
-import { MuiSwitch } from './components/MuiSwitch';
+import { MuiRating } from './components/MuiRating';
+//import { MuiSwitch } from './components/MuiSwitch';
 //import { MuiCheckbox } from './components/MuiCheckbox';
 //import { MuiRadioButton } from './components/MuiRadioButton';
 //import { MuiSelect } from './components/MuiSelect';
@@ -16,7 +17,8 @@ function App() {
       {/* <MuiSelect/> */}
       {/* <MuiRadioButton/> */}
       {/* <MuiCheckbox/> */}
-      <MuiSwitch/>
+      {/* <MuiSwitch/> */}
+      <MuiRating/>
     </div>
   );
 }
